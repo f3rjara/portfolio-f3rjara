ced911a48c2d07f26c833e09c114a9d89bc56dcf
Fernando Jaramillo <f3rjara@gmail.com>
2022-08-18 16:42:12 -0500

    Remove cursor custom for app

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -39,27 +39,4 @@ export class AppComponent implements OnInit {
     if (event instanceof NavigationCancel) { this.showOverlay = false;  }
     if (event instanceof NavigationError)  { this.showOverlay = false;  }
   }
-  
-  
-  /* @HostListener('document:click', ['$event'])
-    onClick($event:any) {
-      this.expand=true;
-      setTimeout(() => {
-      this.expand=false;
-      }, 500)
-    }
-
-  @HostListener('document:mousemove', ['$event'])
-    onMousemove($event:any) {
-      this.top=($event.pageY - 8)+ "px";
-      this.left= ($event.pageX - 8)+ "px";
-    }
-
-  @HostListener('document:mouseover', ['$event'])
-    onMouseOver($event:any) {
-      this.expand=true;
-      setTimeout(() => {
-      this.expand=false;
-      }, 100)
-    } */
 }
